cfbed7d2601997f0622fbef512c309f66491509a
Renjie Wu <rxw453@case.edu>
2020-02-21 17:36:58 -0500

    add a script to config switches

---
+++ switches/switch_conf.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo 'rjwu:1229' | chpasswd
+echo '1229' | sudo -u rjwu chsh -s /bin/bash
+
+cd ./ssh_script
+./ssh_config.sh
+
+cd ../hosts_script
+./change_hosts.sh
+
+cd ../env_scripts
+./ubuntu_python.sh
+
+cd ../ovs_scripts
+./install_ovs.sh
