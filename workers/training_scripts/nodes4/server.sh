87af0fc773649f84d2e1bd1352952b57b9a4dbb4
Renjie Wu <rxw453@case.edu>
2020-02-21 21:29:37 -0500

    modify training scripts

--- workers/training_scripts/nodes4/server.sh
+++ workers/training_scripts/nodes4/server.sh
@@ -1,4 +1,4 @@
 export COMMAND='python image_classification.py --dataset cifar10 --model vgg11 --batch-size 64 --epochs 1 --kvstore dist_sync --log-interval 5'
-cd /tmp/mxnet_job
-DMLC_ROLE=server DMLC_PS_ROOT_URI=10.10.1.8 DMLC_NODE_HOST=10.10.1.8 DMLC_PS_ROOT_PORT=9092 DMLC_NUM_SERVER=1 DMLC_NUM_WORKER=3 $COMMAND &
+cd ~/SketchDLC/example/gluon
+DMLC_ROLE=server DMLC_PS_ROOT_URI=10.10.1.4 DMLC_NODE_HOST=10.10.1.4 DMLC_PS_ROOT_PORT=9092 DMLC_NUM_SERVER=1 DMLC_NUM_WORKER=3 $COMMAND &
 
