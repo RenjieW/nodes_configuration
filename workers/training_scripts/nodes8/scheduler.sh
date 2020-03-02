8a1c6573f24eb53cc1b9c9d21f6d0d1d75f2b0a5
Renjie Wu <rxw453@case.edu>
2020-03-02 13:51:14 -0500

    modify the format

--- workers/training_scripts/nodes8/scheduler.sh
+++ workers/training_scripts/nodes8/scheduler.sh
@@ -1,4 +1,3 @@
 export COMMAND='python image_classification.py --dataset cifar10 --model vgg11 --batch-size 64 --epochs 1 --kvstore dist_sync --log-interval 5'
 cd ~/SketchDLC/example/gluon
 DMLC_ROLE=scheduler DMLC_PS_ROOT_URI=10.10.1.8 DMLC_NODE_HOST=10.10.1.8 DMLC_PS_ROOT_PORT=9092 DMLC_NUM_SERVER=2 DMLC_NUM_WORKER=6 $COMMAND &
-
