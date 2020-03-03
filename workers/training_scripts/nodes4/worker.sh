abaf03591826b47048722908a7a3b82af3e0d446
Renjie Wu <rxw453@case.edu>
2020-03-03 13:46:32 -0500

    add a new script

--- workers/training_scripts/nodes4/worker.sh
+++ workers/training_scripts/nodes4/worker.sh
@@ -1,3 +1,4 @@
 export COMMAND='python image_classification.py --dataset cifar10 --model vgg11 --batch-size 64 --epochs 1 --kvstore dist_sync --log-interval 5'
 cd ~/SketchDLC/example/gluon
+cd /tmp/mxnet_job
 DMLC_ROLE=worker DMLC_PS_ROOT_URI=10.10.1.4 DMLC_NODE_HOST=10.10.1.1 DMLC_PS_ROOT_PORT=9092 DMLC_NUM_SERVER=1 DMLC_NUM_WORKER=3 $COMMAND &
